750e37822294702c0ae8f6c005663b57fbd2b4b6
Antek <antoni.samol@gmail.com>
2025-11-04 21:39:38 +0100

    weather API 1st commit, work in progress

--- main.py
+++ main.py
@@ -2,3 +2,28 @@ import sys
 import requests
 from PyQt5.QtWidgets import (QApplication, QLabel, QPushButton,
                              QLineEdit, QVBoxLayout, QWidget)
+from PyQt5.QtCore import Qt
+
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter City Name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel("70C", self)
+        self.emoji_label = QLabel("☀️", self)
+        self.description_label = QLabel("Sunny", self)
+
+    def initUI(self):
+        self.setWindowTitle("Weather App")
+
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(self.city_label)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
